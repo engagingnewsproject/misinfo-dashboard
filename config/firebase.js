8165fa11580dc0ff2113a4660bb9d5f11267fe6e
Shaz Momin <shaz.momin@utexas.edu>
2022-06-08 23:38:08 -0500

    Authentication setup with firebase

--- config/firebase.js
+++ config/firebase.js
@@ -1,3 +1,17 @@
-// TODO: Add all the firebase config stuff here after setting up firebase
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBkD07zb1NS6J8Um617Bxz2O16E0IOgB0Y",
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
