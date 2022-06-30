6bfc3b57e08b6c6289c5ef03c1c846b6f20e2bb6
Shaz Momin <shaz.momin@utexas.edu>
2022-06-30 15:41:08 -0500

    dashboard (home page) boilerplate setup

---
+++ components/ChartsSection.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const chartContainer = "bg-white flex justify-center items-center rounded-xl"
+
+const ChartsSection = () => {
+  return (
+    <div class="flex flex-col">
+        <div class="text-lg font-bold text-blue-600 tracking-wider py-5">Top 3 Trendy Tag</div>
+        <div class="grid grid-cols-3 gap-10 item-center pb-5 h-52 text-sm">
+            <div class={chartContainer}>
+                <div>No Data</div>
+            </div>
+            <div class={chartContainer}>
+                <div>No Data</div>
+            </div>
+            <div class={chartContainer}>
+                <div>No Data</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChartsSection
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Headbar from '../components/Headbar'
+import ChartsSection from './ChartsSection'
+import ReportsSection from './ReportsSection'
 
+const views = ["Overview", "Comparison View"]
 
 const Home = () => {
+  const [viewVal, setViewVal] = useState(0)
+
+  const handleChange = (e) => {
+    const {value} = e.target
+    setViewVal(value)
+  }
+
+  const styles = {
+    checked: "bg-blue-600 text-white py-2 px-5 shadow-sm text-sm font-light tracking-wide",
+    unchecked: "bg-white py-2 px-5 shadow-sm text-sm font-light tracking-wide"
+  }
+
   return (
-    <div class="w-full h-full flex-col py-5">
+    <div class="w-full h-full flex flex-col py-5">
         <Headbar />
+        <div class="w-full flex flex-col px-12 py-5">
+          <div class="flex justify-between mb-2">
+            <form onChange={handleChange}>
+              <input type="radio" id="overview" defaultChecked value="0" name="view" class="hidden" />
+              <label for="overview"
+                class={viewVal == 0 ? styles.checked : styles.unchecked}>
+                Overview
+              </label>
+              <input type="radio" id="comparison view" value="1" name="view" class="hidden"/>
+              <label for="comparison view"
+                class={viewVal == 1 ? styles.checked : styles.unchecked}>
+                Comparison View
+              </label>
+            </form>
+            <div>Dates select</div>
+          </div>
+          <ChartsSection />
+          <ReportsSection />
+        </div>
     </div>
   )
 }
