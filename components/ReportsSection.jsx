64050f8961d67414e9f56c31d90f13dde8a018ec
Shaz Momin <shaz.momin@utexas.edu>
2022-07-27 16:01:21 -0500

    reporter details, images, & multi-links added

--- components/ReportsSection.jsx
+++ components/ReportsSection.jsx
@@ -11,6 +11,10 @@ const ReportsSection = () => {
 
   const tableHeadings = "p-2 text-center text-sm font-semibold tracking-wide"
   const columnData = "text-center text-sm px-2 py-1 flex items-center justify-center"
+  const label = {
+    default: "overflow-hidden inline-block px-5 bg-gray-200 py-1 rounded-2xl",
+    special: "overflow-hidden inline-block px-5 bg-yellow-400 py-1 rounded-2xl"
+  }
 
   const getData = async() => {
     const reportsCollection = collection(db, "reports")
@@ -38,7 +42,7 @@ const ReportsSection = () => {
   return (
     <div class="flex flex-col">
       <div class="text-lg font-bold text-blue-600 tracking-wider py-5">List of Reports</div>
-      <div class="bg-white w-full rounded-xl p-1 overflow-auto h-72">
+      <div class="bg-white w-full rounded-xl p-1 h-72">
         <div class="grid grid-cols-7">
           <div class={"col-span-2 " + tableHeadings}>Title</div>
           <div class={tableHeadings}>Date/Time</div>
@@ -47,25 +51,30 @@ const ReportsSection = () => {
           <div class={tableHeadings}>Sources</div>
           <div class={tableHeadings}>Labels</div>
         </div>
-        {reports.map((reportObj) => {
-          const report = Object.values(reportObj)[0]
-          const options = { day: '2-digit', year: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric' }
-          const posted = report["createdDate"].toDate().toLocaleString('en-US', options).replace(/,/g,"").replace('at', '')
-          return (
-            <Link href={`/dashboard/reports/${Object.keys(reportObj)[0]}`}>
-              <a target="_blank" class="grid grid-cols-7 hover:bg-blue-200">
-                <div class={"col-span-2 truncate " + columnData}>{report.title}</div>
-                <div class={columnData}>{posted}</div>
-                <div class={columnData}>None</div>
-                <div class={columnData}>{report.topic}</div>
-                <div class={columnData}>{report.source}</div>
-                <div class={columnData}>
-                  <div class="overflow-hidden inline-block px-5 bg-yellow-400 py-1 rounded-2xl">{report.label || "None"}</div>
-                </div>
-              </a>
-            </Link>
-          )
-        })}
+        <div class="overflow-auto h-60">
+          {reports
+            .sort((objA, objB) => Object.values(objA)[0]["createdDate"] > Object.values(objB)[0]["createdDate"] ? -1 : 1)
+            .map((reportObj) => {
+            const report = Object.values(reportObj)[0]
+            const options = { day: '2-digit', year: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric' }
+            const posted = report["createdDate"].toDate().toLocaleString('en-US', options).replace(/,/g,"").replace('at', '')
+            return (
+              <Link href={`/dashboard/reports/${Object.keys(reportObj)[0]}`}>
+                <a target="_blank" class="grid grid-cols-7 hover:bg-blue-200">
+                  <div class={"col-span-2 truncate " + columnData}>{report.title}</div>
+                  <div class={columnData}>{posted}</div>
+                  <div class={columnData}>-</div>
+                  <div class={columnData}>{report.topic}</div>
+                  <div class={columnData}>{report.hearFrom}</div>
+                  <div class={columnData}>
+                    <div class={!report.label ? label.default : label.special}>{report.label || "None"}</div>
+                  </div>
+                </a>
+              </Link>
+            )
+          })}
+        </div>
+        
       </div>
     </div>
   )
