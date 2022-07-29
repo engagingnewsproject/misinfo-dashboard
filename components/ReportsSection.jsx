85b897728daed21de4b8720a6fc6676b306aabee
Shaz Momin <shaz.momin@utexas.edu>
2022-07-29 14:40:37 -0500

    searching the reports feature added

--- components/ReportsSection.jsx
+++ components/ReportsSection.jsx
@@ -4,10 +4,12 @@ import { collection, listCollections, getDoc, getDocs, doc } from "firebase/fire
 import { db } from '../config/firebase'
 import Link from 'next/link'
 
-const ReportsSection = () => {
+const ReportsSection = ({ search }) => {
 
   const [reports, setReports] = useState([])
+  const [filteredReports, setFilteredReports] = useState([])
   const { user } = useAuth()
+  const dateOptions = { day: '2-digit', year: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric' }
 
   const tableHeadings = "p-2 text-center text-sm font-semibold tracking-wide"
   const columnData = "text-center text-sm px-2 py-1 flex items-center justify-center"
@@ -27,17 +29,44 @@ const ReportsSection = () => {
           [doc.id]: doc.data(),
         })
       })
-      //console.log(arr)
+
       setReports(arr)
+      setFilteredReports(arr)
     } catch (error) {
       console.log(error)
     }
   }
 
+  // Filter the reports based on the search text
+  useEffect(() => {
+    setFilteredReports(reports.filter((reportObj) => {
+      const report = Object.values(reportObj)[0]
+      var arr = []
+      // Collect the searchable fields of the reports data
+      for (const key in report) {
+        if (report[key]) {
+          if (key != "images" && key != "userID") {
+            if (key == "createdDate") {
+              const posted = report[key].toDate().toLocaleString('en-US', dateOptions).replace(/,/g,"").replace('at', '')
+              arr.push(posted.toLowerCase())
+            } else {
+              arr.push(report[key].toString().toLowerCase())
+            }
+          }
+        }
+      }
+
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i].includes(search.toLowerCase())) return true
+      }
+    }))
+  }, [search])
+
   // On page load (mount), get the reports from firebase
   useEffect(() => {
     getData()
   }, [])
+
 
   return (
     <div class="flex flex-col">
@@ -51,17 +80,16 @@ const ReportsSection = () => {
           <div class={tableHeadings}>Sources</div>
           <div class={tableHeadings}>Labels</div>
         </div>
-        <div class="overflow-auto h-60">
-          {reports
+        <div class="overflow-auto h-56 md:h-60">
+          {filteredReports
             .sort((objA, objB) => Object.values(objA)[0]["createdDate"] > Object.values(objB)[0]["createdDate"] ? -1 : 1)
             .map((reportObj) => {
             const report = Object.values(reportObj)[0]
-            const options = { day: '2-digit', year: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric' }
-            const posted = report["createdDate"].toDate().toLocaleString('en-US', options).replace(/,/g,"").replace('at', '')
+            const posted = report["createdDate"].toDate().toLocaleString('en-US', dateOptions).replace(/,/g,"").replace('at', '')
             return (
               <Link href={`/dashboard/reports/${Object.keys(reportObj)[0]}`}>
                 <a target="_blank" class="grid grid-cols-7 hover:bg-blue-200">
-                  <div class={"col-span-2 truncate " + columnData}>{report.title}</div>
+                  <div class={"col-span-2 " + columnData}>{report.title}</div>
                   <div class={columnData}>{posted}</div>
                   <div class={columnData}>-</div>
                   <div class={columnData}>{report.topic}</div>
