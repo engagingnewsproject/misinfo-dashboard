be57c2ca404a7133e77946f969b766103d42a29f
julia-elias <jjelias03@gmail.com>
2022-10-17 13:47:34 -0500

    added overview and comparison view buttons

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Headbar from '../components/Headbar'
+import Toggle from '../components/Toggle'
 import ReportsSection from './ReportsSection'
 
-const views = ["Overview", "Comparison View"]
-
 const Home = () => {
-  const [viewVal, setViewVal] = useState(0)
+  const [viewVal, setViewVal] = useState("overview")
   const [search, setSearch] = useState("")
-
-  const handleChange = (e) => {
-    const {value} = e.target
-    setViewVal(value)
-  }
 
   const styles = {
     checked: "bg-blue-600 text-white py-2 px-5 shadow-sm text-sm font-light tracking-wide",
@@ -21,7 +15,12 @@ const Home = () => {
   return (
     <div class="w-full h-full flex flex-col py-5">
         <Headbar search={search} setSearch={setSearch} />
+        {/* Nest the following toggle inside a component for the graph portion of the page*/}
         <div class="w-full h-full flex flex-col px-12 py-5 mb-5 overflow-y-hidden">
+          <Toggle viewVal={viewVal} setViewVal={setViewVal}/> 
+          {/* Following code will be used to determing which display to show. 
+              Components need to be made for each display.*/}
+          {/* viewVal == "overview" ? <overviewDisplay/> : <comparisonDisplay/> */}
           <ReportsSection search={search} />
         </div>
     </div>
