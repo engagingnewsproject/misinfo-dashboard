4f2766a580d78d133386f74be05cd5d4a164184a
Luke Carl Hartman <luke@lukecarlhartman.com>
2022-11-03 19:15:17 -0700

    navbar tabs update v1

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import { MdHomeFilled } from 'react-icons/md';
 import { CgProfile } from 'react-icons/cg'
-import { IoSettingsSharp } from 'react-icons/io5'
+import { IoSettingsSharp, IoAddCircleSharp, IoPricetagsSharp } from "react-icons/io5";
 import { BiLogOut } from 'react-icons/bi'
 import { useAuth } from '../context/AuthContext';
 import ConfirmModal from './modals/ConfirmModal';
@@ -11,12 +11,17 @@ const Navbar = ({tab, setTab}) => {
 
     const { logout } = useAuth()
     const router = useRouter()
+    const [reportModal, setReportModal] = useState(false);
     const [logoutModal, setLogoutModal] = useState(false)
 
     const handleLogout = () => {
         logout()
         router.push('/login')
     }
+
+    const handleReport = () => {
+        alert('REPORT');
+    };
 
     const basicStyle = "flex p-2 my-6 mx-2 justify-center text-gray-500 hover:bg-indigo-100 rounded-lg"
 
@@ -26,23 +31,33 @@ const Navbar = ({tab, setTab}) => {
                 <div>
                     <button 
                         onClick={() => setTab(0)}
+                        title="Home"
                         class={tab == 0 ? basicStyle + " text-indigo-500 bg-indigo-100" : basicStyle}>
                         <MdHomeFilled size={30}/>
                     </button>
                     <button
-                        onClick={() => setTab(1)}
-                        class={tab == 1 ? basicStyle + " text-indigo-500 bg-indigo-100" : basicStyle}>
-                        <CgProfile size={30}/>
+                        onClick={() => setTab(2)}
+                        title="Tagging Systems"
+                        class={tab == 2 ? basicStyle + " text-indigo-500 bg-indigo-100" : basicStyle}>
+                        <IoPricetagsSharp size={30}/>
                     </button>
                     <button
-                        onClick={() => setTab(2)}
-                        class={tab == 2 ? basicStyle + " text-indigo-500 bg-indigo-100" : basicStyle}>
-                        <IoSettingsSharp size={30}/>
+                        onClick={() => setReportModal(true)}
+                        title="New Report"
+                        class={basicStyle}>
+                        <IoAddCircleSharp size={30}/>
                     </button>
                 </div>
                 <div>
                     <button
+                        onClick={() => setTab(1)}
+                        title="Profile"
+                        class={tab == 1 ? basicStyle + " text-indigo-500 bg-indigo-100" : basicStyle}>
+                        <IoSettingsSharp size={30}/>
+                    </button>
+                    <button
                         onClick={() => setLogoutModal(true)}
+                        title="Logout"
                         class={basicStyle}>
                         <BiLogOut size={30}/>
                     </button>
@@ -54,6 +69,13 @@ const Navbar = ({tab, setTab}) => {
                 subtitle=""
                 CTA="Log out"
                 closeModal={setLogoutModal}
+                />}
+            { reportModal && <ConfirmModal
+                func={handleReport}
+                title="New Report"
+                subtitle=""
+                CTA="Log report"
+                closeModal={setReportModal}
                 />}
         </div>
     )
