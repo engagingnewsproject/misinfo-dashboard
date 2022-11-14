827a9405487dda9be3a761215a89ea7c8cf46244
julia-elias <jjelias03@gmail.com>
2022-11-14 17:40:29 -0600

    responsive design for pie charts

--- components/Toggle.jsx
+++ components/Toggle.jsx
@@ -1,15 +1,15 @@
 /* Generates the toggle that determines whether the dashboard should display
 the comparison view or overview of the graphs for trending tag topics.*/
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 const Toggle = ({ viewVal, setViewVal }) => {   
     
-    {/*Sets the view to overview or comparison based on which button was clicked*/}
+    // Sets the view to overview or comparison based on which button was clicked
     const handleViewChanged = (e) => {
         setViewVal(e.target.id)
     }
     
-    {/*Style settings used for the button that is active and the nonactive button*/}
+    // Style settings used for the button that is active and the nonactive button 
     const active = "bg-blue-600 text-white py-2 px-5 drop-shadow-lg text-sm font-light tracking-wide"
     const nonactive = "bg-white py-2 px-5 drop-shadow-lg text-sm font-light tracking-wide"
     return (
