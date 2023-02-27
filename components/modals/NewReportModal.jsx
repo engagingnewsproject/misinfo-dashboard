09f5f42952ed3d7916a7b2465a268bf92c03da68
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-02-27 11:33:25 -0800

    new report modal overlay click close

--- components/modals/NewReportModal.jsx
+++ components/modals/NewReportModal.jsx
@@ -13,7 +13,8 @@ import Select from "react-select";
 // Ref to firebase reports collection
 const dbInstance = collection(db, 'reports');
 
-const NewReport = ({ setNewReport, addNewReport }) => {
+const NewReport = ({ open, onClose }) => {
+    if (!open) return null
     const router = useRouter()
     const { user } = useAuth()
     // useStates
@@ -48,7 +49,7 @@ const NewReport = ({ setNewReport, addNewReport }) => {
     }
 
     const handleChange = (e) => {
-        // console.log({ ...e, [e.target.id]: e.target.value})
+        console.log(e.target.value)
     }
     
     const handleNewReport = async (e) => {
@@ -71,122 +72,118 @@ const NewReport = ({ setNewReport, addNewReport }) => {
 
     return (
         <div>
-            <div class="flex justify-center items-center z-10 absolute top-0 left-0 w-full h-full bg-black opacity-60">
-            </div>
-            <div class="flex justify-center items-center z-20 absolute top-0 left-0 w-full h-full">
-                <div class="flex-col justify-center items-center bg-white w-1/3 h-auto rounded-2xl py-10 px-10">
-                    <div class="flex justify-between w-full mb-5">
-                        <div class="text-md font-bold text-blue-600 tracking-wide">Add New Report</div>
-                        <button onClick={() => setNewReport(false)} class="text-gray-800">
-                            <IoClose size={25}/>
-                        </button>
-                    </div>
-                    <form 
-                        onChange={handleChange} 
-                        onSubmit={handleNewReport}
-                        >
-
-                        <div class="mb-4">
-                            <Select
-                                class="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="state"
-                                type="text"
-                                placeholder="State"
-                                value={data.state}
-                                options={State.getStatesOfCountry(data.country)}
-                                getOptionLabel={(options) => {
-                                  return options["name"];
-                                }}
-                                getOptionValue={(options) => {
-                                  return options["name"];
-                                }}                                
-                                label="state"
-                                onChange={(value => {
-                                  setData(data=>({...data, state: value, city: null })) 
-                                  })}
-
-                                />        
-
-                        </div>
-                        <div class="mb-0.5">
-                            <Select
-                                class="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="city"
-                                type="text"
-                                placeholder="City"
-                                value={data.city}
-                                options={City.getCitiesOfState(
-                                  data.state?.countryCode,
-                                  data.state?.isoCode
-                                )}
-                                getOptionLabel={(options) => {
-                                  return options["name"];
-                                }}
-                                getOptionValue={(options) => {
-                                  return options["name"];
-                                }}                                 
-                                onChange={(value => {
-                                setData(data=>({...data, city: value})) 
-                                })}
-                               
-                                />
-                        </div>
-
-                        <div class="mt-4 mb-0.5">
-                            <input
-                                class="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="title"
-                                type="text"
-                                placeholder="Report Title"
-                                required
-                                onChange={(e) => setTitle(e.target.value)}
-                                value={title}
-                                />
-                        </div>
-                        <div class="mt-4 mb-0.5">
-                            <input
-                                class="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="link"
-                                type="text"
-                                placeholder="Link"
-                                required
-                                onChange={(e) => setLink(e.target.value)}
-                                value={link}
-                                />
-                        </div>
-                        <div class="mt-4 mb-0.5">
-                            <input
-                                class="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="secondLink"
-                                type="text"
-                                placeholder="Second Link"
-                                onChange={(e) => setSecondLink(e.target.value)}
-                                value={secondLink}
-                                />
-                        </div>
-                        <div class="mt-4 mb-0.5">
-                            <input
-                                class="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="detail"
-                                type="text"
-                                placeholder="Detail"
-                                required
-                                onChange={(e) => setDetail(e.target.value)}
-                                value={detail}
-                                />
-                        </div>
-                        <div className='mt-4 mb-0.5'>
-                            <label class="absolute invisible" for="multiple_files">Upload multiple files</label>
-                            <input class="block shadow w-full text-sm rounded-md cursor-pointer text-gray-700 dark:text-gray-400 focus:outline-none dark:placeholder-gray-400" id="multiple_files" type="file" multiple accept="image/*" onChange={onImageChange} multiple />
-                            { imageURLs.map(imageSrc => <img src={imageSrc} className="py-3 h-auto shadow" />) }
-                        </div>
-                        <div class="mt-6">
-                            <button
-                                class="w-full bg-blue-500 hover:bg-blue-700 text-sm text-white font-semibold py-2 px-6 rounded-md focus:outline-none focus:shadow-outline" type="submit">
-                                Create
+            <div className="z-10 fixed top-0 left-0 w-full h-full bg-black bg-opacity-50">
+                <div onClick={onClose} className="flex overflow-y-auto justify-center items-center z-20 absolute top-0 left-0 w-full h-full">
+                    <div onClick={(e) => {e.stopPropagation()}} className="flex-col justify-center items-center bg-white w-6/12 h-auto rounded-2xl py-10 px-10">
+                        <div className="flex justify-between w-full mb-5">
+                            <div className="text-md font-bold text-blue-600 tracking-wide">Add New Report</div>
+                            <button onClick={onClose} className="text-gray-800">
+                                <IoClose size={25}/>
                             </button>
                         </div>
-                    </form>
+                        <form onChange={handleChange} onSubmit={handleNewReport}>
+                            <div className="mb-4">
+                                <Select
+                                    className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="state"
+                                    type="text"
+                                    placeholder="State"
+                                    value={data.state}
+                                    options={State.getStatesOfCountry(data.country)}
+                                    getOptionLabel={(options) => {
+                                    return options["name"];
+                                    }}
+                                    getOptionValue={(options) => {
+                                    return options["name"];
+                                    }}                                
+                                    label="state"
+                                    onChange={(value => {
+                                    setData(data=>({...data, state: value, city: null })) 
+                                    })}
+
+                                    />        
+
+                            </div>
+                            <div className="mb-0.5">
+                                <Select
+                                    className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="city"
+                                    type="text"
+                                    placeholder="City"
+                                    value={data.city}
+                                    options={City.getCitiesOfState(
+                                    data.state?.countryCode,
+                                    data.state?.isoCode
+                                    )}
+                                    getOptionLabel={(options) => {
+                                    return options["name"];
+                                    }}
+                                    getOptionValue={(options) => {
+                                    return options["name"];
+                                    }}                                 
+                                    onChange={(value => {
+                                    setData(data=>({...data, city: value})) 
+                                    })}
+                                
+                                    />
+                            </div>
+
+                            <div className="mt-4 mb-0.5">
+                                <input
+                                    className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="title"
+                                    type="text"
+                                    placeholder="Report Title"
+                                    required
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    value={title}
+                                    />
+                            </div>
+                            <div className="mt-4 mb-0.5">
+                                <input
+                                    className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="link"
+                                    type="text"
+                                    placeholder="Link"
+                                    required
+                                    onChange={(e) => setLink(e.target.value)}
+                                    value={link}
+                                    />
+                            </div>
+                            <div className="mt-4 mb-0.5">
+                                <input
+                                    className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="secondLink"
+                                    type="text"
+                                    placeholder="Second Link"
+                                    onChange={(e) => setSecondLink(e.target.value)}
+                                    value={secondLink}
+                                    />
+                            </div>
+                            <div className="mt-4 mb-0.5">
+                                <input
+                                    className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="detail"
+                                    type="text"
+                                    placeholder="Detail"
+                                    required
+                                    onChange={(e) => setDetail(e.target.value)}
+                                    value={detail}
+                                    />
+                            </div>
+                            <div className='mt-4 mb-0.5'>
+                                <label className="absolute invisible" for="multiple_files">Upload multiple files</label>
+                                <input className="block shadow w-full text-sm rounded-md cursor-pointer text-gray-700 dark:text-gray-400 focus:outline-none dark:placeholder-gray-400" id="multiple_files" type="file" multiple accept="image/*" onChange={onImageChange} multiple />
+                                { imageURLs.map(imageSrc => <img src={imageSrc} className="py-3 h-auto shadow" />) }
+                            </div>
+                            <div className="mt-6">
+                                <button
+                                    className="w-full bg-blue-500 hover:bg-blue-700 text-sm text-white font-semibold py-2 px-6 rounded-md focus:outline-none focus:shadow-outline" type="submit">
+                                    Create
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
