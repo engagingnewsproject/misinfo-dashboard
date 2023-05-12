e0cf2ef73548a21c1fa248a2f4aca37f24baf12e
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-05-12 09:31:06 -0700

    report list responsive & tooltips

--- components/ReportsSection.jsx
+++ components/ReportsSection.jsx
@@ -497,10 +497,10 @@ const ReportsSection = ({ search, newReportSubmitted, handleNewReportSubmit }) =
 	return (
 		<div className="flex flex-col h-full">
 			<div className="flex flex-col md:flex-row py-5 md:justify-between">
-				<div className="text-lg font-bold text-blue-600 tracking-wider">
+				<div className="text-center md:text-left text-lg font-bold text-blue-600 tracking-wider pb-2 md:pb-0">
 					List of Reports
 				</div>
-				<div className="flex flex-row justify-between md:justify-evenly">
+				<div className="flex flex-row flex-wrap md:flex-nowrap items-center justify-center md:justify-evenly">
           <div className="px-4">
             <ReactTooltip
                 id="refreshTooltip"
@@ -511,7 +511,7 @@ const ReportsSection = ({ search, newReportSubmitted, handleNewReportSubmit }) =
               />
               {/* Displays refresh icon */}
               {!refresh && !reportsUpdated && <button
-                className="relative top-1"
+                className="relative top-1 m-3 md:m-0"
                 onClick={handleRefresh}
                 data-tip="Refresh"
                 data-for="refreshTooltip">
@@ -522,8 +522,8 @@ const ReportsSection = ({ search, newReportSubmitted, handleNewReportSubmit }) =
               {refresh && !reportsUpdated && 
               <div>
                 <svg aria-hidden="true" className="ml-2 w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">	
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />	
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />	
+									<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />	
+									<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />	
                 </svg>
               </div>}      
 
@@ -534,31 +534,59 @@ const ReportsSection = ({ search, newReportSubmitted, handleNewReportSubmit }) =
             </div>}
           </div>
 					<div>
+							{/* New report tooltip */}
+							<ReactTooltip
+                id="newReportTooltip"
+                place="top"
+                type="light"
+                effect="solid"
+                delayShow={500}
+              />
             <button
               onClick={() => setNewReportModal(true)}
-              className="text-sm bg-white px-4 border-none shadow text-black py-1 rounded-md hover:shadow-none active:bg-white focus:outline-none focus:ring-1 focus:ring-blue-600">
-              <div className="flex items-center">
+              className="flex items-center text-sm bg-white px-4 border-none shadow text-black py-1 rounded-md hover:shadow-none active:bg-white focus:outline-none focus:ring-1 focus:ring-blue-600"
+							data-tip="Create a new report"
+							data-for="newReportTooltip">
               <IoAdd className="mr-1" size={15} />
               New Report
-              </div>
             </button>
           </div>
 				  <div>
+						{/* Filter tooltip */}
+						<ReactTooltip
+							id="filterTooltip"
+							place="top"
+							type="light"
+							effect="solid"
+							delayShow={500}
+						/>
             <select
               id="labels"
               onChange={(e) => handleReadFilterChanged(e)}
               defaultValue="All"
-              className="text-sm font-semibold shadow bg-white inline-block px-8 border-none text-black py-1 rounded-md md:mx-2 hover:shadow-none">
+							data-tip="Filter reports"
+							data-for="filterTooltip"
+              className="text-sm font-semibold shadow bg-white inline-block px-8 border-none text-black py-1 rounded-md mr-1 md:mx-2 hover:shadow-none">
               <option value="false">Unread</option>
               <option value="true">Read</option>
               <option value="All">All reports</option>
             </select>
           </div>
           <div>
+						{/* Timeframe tooltip */}
+						<ReactTooltip
+							id="timeframeTooltip"
+							place="top"
+							type="light"
+							effect="solid"
+							delayShow={500}
+						/>
             <select
               id="labels"
               onChange={(e) => handleDateChanged(e)}
               defaultValue="4"
+							data-tip="Select timeframe"
+							data-for="timeframeTooltip"
               className="text-sm font-semibold shadow bg-white inline-block px-8 border-none text-black py-1 rounded-md hover:shadow-none">
               <option value="4">Last four weeks</option>
               <option value="3">Last three weeks</option>
@@ -612,10 +640,20 @@ const ReportsSection = ({ search, newReportSubmitted, handleNewReportSubmit }) =
 										<td className={column.data}>{report.topic}</td>
 										<td className={column.data}>{report.hearFrom}</td>
 										<td className={column.data_center}>
+											{/* Change label tooltip */}
+											<ReactTooltip
+												id="labelTooltip"
+												place="top"
+												type="light"
+												effect="solid"
+												delayShow={500}
+											/>
 											<div
 												className={
 													!report.label ? label.default : label.special
-												}>
+												}
+												data-tip="Change label"
+												data-for="labelTooltip">
 												{report.label || "None"}
 											</div>
 										</td>
