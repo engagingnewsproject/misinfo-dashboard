999124ba7088fd451e0741515cadbb860e65c62b
julia-elias <jjelias03@gmail.com>
2023-08-03 11:35:20 -0500

    debugging verifying a user's role

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -59,87 +59,11 @@ export const AuthContextProvider = ({children}) => {
 
 
     // add admin cloud function
-
     const addAdminRole = httpsCallable(functions, 'addAdminRole')
 
+    const addAgencyRole = httpsCallable(functions, 'addAgencyRole')
 
-    const verifyPrivilege = async (uid) => {
-      auth
-      .getUserByEmail(uid)
-        .then((userRecord) => {
-        // The claims can be accessed on the user record.
-        console.log(userRecord.customClaims['admin']);
-
-        let customClaims = {
-          admin: false,
-          agency: false
-        }
-        if (userRecord.customClaims['admin'] || userRecord.customClaims['agency']) {
-          if (userRecord.customClaims['admin'] || userRecord.customClaims['agency']) {
-            customClaims = {
-              admin: true,
-              agency: true
-            }
-          } else if (userRecord.customClaims['admin']) {
-            customClaims = {
-              admin: true,
-              agency: false
-            }
-          } else if (userRecord.customClaims['agency']) {
-            customClaims = {
-              admin: false,
-              agency: true
-            }
-          } 
-        } else {
-          return customClaims;
-        }
-        
-      });
-    }
-     const changeRole = async function (uid, role) {
-      let customClaims = {
-        admin: false,
-        agency: false
-      }
-
-      if (uid && uid === "juliaelias@utexas.edu") {
-        customClaims = {
-          admin: true,
-          agency: false
-        };
-      } else {
-        customClaims = {
-          admin: false,
-          agency: false
-        }
-        console.log("i am in here")
-      }
-
-      try {
-        // Set custom user claims on this newly created user.
-        await auth.setCustomUserClaims(user.uid, customClaims);
-  
-        // Update real-time database to notify client to force refresh.
-        const metadataRef = getDatabase().ref('metadata/' + user.uid);
-  
-        // Set the refresh time to the current UTC timestamp.
-        // This will be captured on the client to force a token refresh.
-        await  metadataRef.set({refreshTime: new Date().getTime()});
-
-        
-      } catch (error) {
-        console.log(error);
-      }
-
-      return auth.getUserByEmail(uid)
-      .then((userRecord) => {
-        // The claims can be accessed on the user record.
-        return userRecord.customClaims['admin'];
-      });
-
-    }
-
+    const viewRole = httpsCallable(functions, 'viewRole')
 
     const signup = (teamName, email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
@@ -167,7 +91,7 @@ export const AuthContextProvider = ({children}) => {
     }
  
     return (
-        <AuthContext.Provider value={{ user, login, signup, logout, resetPassword, updatePassword, addAdminRole, changeRole, verifyPrivilege }}>
+        <AuthContext.Provider value={{ user, login, signup, logout, resetPassword, updatePassword, addAdminRole, addAgencyRole, viewRole }}>
             {loading ? null : children}
         </AuthContext.Provider>
     )
