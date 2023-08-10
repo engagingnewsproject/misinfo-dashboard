babaa24300936fda2c4d444f81b1b09c513028a2
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-08-10 16:45:03 -0700

    INIT dev firebase project creds

--- config/firebase.js
+++ config/firebase.js
@@ -20,14 +20,22 @@ const firebaseConfig = {
     appId: "1:10043942539:web:4d2ab14272000c38f4d68e",
     measurementId: "G-EJ0G2CVELW" */
 
-    /* NEW combined database keys */
-    apiKey: "AIzaSyAsGCi7VgxuovHAbY4tRDAKDRN6sxw8MHo",
-    authDomain: "misinfo-5d004.firebaseapp.com",
-    projectId: "misinfo-5d004",
-    storageBucket: "misinfo-5d004.appspot.com",
-    messagingSenderId: "2581605663",
-    appId: "1:2581605663:web:5c1f1a43d80568fd5b542a",
-    measurementId: "G-L4GJJGV0V1"
+    /* LIVE Misinfo (misinfo-5d004) app creds */
+    // apiKey: "AIzaSyAsGCi7VgxuovHAbY4tRDAKDRN6sxw8MHo",
+    // authDomain: "misinfo-5d004.firebaseapp.com",
+    // projectId: "misinfo-5d004",
+    // storageBucket: "misinfo-5d004.appspot.com",
+    // messagingSenderId: "2581605663",
+    // appId: "1:2581605663:web:5c1f1a43d80568fd5b542a",
+    // measurementId: "G-L4GJJGV0V1"
+    
+    // DEV site (misinfo-dashboard-dev) app creds
+    apiKey: "AIzaSyA_yhrMIO9lkS8ytjKTdNbPTWgoex09vCc",
+    authDomain: "misinfo-dashboard-dev.firebaseapp.com",
+    projectId: "misinfo-dashboard-dev",
+    storageBucket: "misinfo-dashboard-dev.appspot.com",
+    messagingSenderId: "1032537406721",
+    appId: "1:1032537406721:web:12713268b158a7843a2f00"
 };
 
 // Initialize Firebase
