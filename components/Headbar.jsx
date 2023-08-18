1197d28a7e78cb32967b4697e74b66cae3d1c539
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-08-18 12:24:43 -0700

    working on getting agency users (per agency)

--- components/Headbar.jsx
+++ components/Headbar.jsx
@@ -1,14 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { collection, getDocs } from '@firebase/firestore'
+import { collection, getDocs, doc, query, where } from '@firebase/firestore'
 import { useAuth } from '../context/AuthContext'
 import { db, auth } from "../config/firebase"
+import { getStorage,ref,getDownloadURL } from "firebase/storage";
+import Image from 'next/image'
 
 const Headbar = ({ search, setSearch, customClaims}) => {
+
     const { user, verifyRole } = useAuth()
     const [userRole, setUserRole] = useState('')
+    const [agencyLogo,setAgencyLogo] = useState('')
+    const [agencies,setAgencies] = ([])
+    const [userId, setUserId] = useState('')
+    const storage = getStorage();
+    const starsRef = ref(storage, 'agencies/agencyDesign/logo_1692381497502.png');
+    getDownloadURL(starsRef)
+    .then((url) => {
+        setAgencyLogo(url)
+    })
+    .catch((error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+        case 'storage/object-not-found':
+            // File doesn't exist
+            break;
+        case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+        case 'storage/canceled':
+            // User canceled the upload
+            break;
+        case 'storage/unknown':
+            // Unknown error occurred, inspect the server response
+            break;
+        }
+    })
     
-    // Verify user role
     useEffect(() => {
         verifyRole().then((result) => {
             if (result.admin) {
@@ -21,12 +50,10 @@ const Headbar = ({ search, setSearch, customClaims}) => {
         })
     }, [])
 
-	// //
-	// Data
-	// //
 	const getData = async () => {
 		const userRoles = collection(db, 'userRoles')
-		const snapshot = await getDocs(userRoles)
+        const snapshot = await getDocs(userRoles)
+        console.log(snapshot)
 		try {
 			var arr = []
 			snapshot.forEach((doc) => {
@@ -36,8 +63,39 @@ const Headbar = ({ search, setSearch, customClaims}) => {
 			})
 		} catch (error) {
 			console.log(error)
-		}
-
+        }
+        // NEW Work
+        // Working on getting all agency users attached to an agency
+        // this is something we will have to do as the project moves along.
+        // We need to somehow relate users, reports, tags, sources & assets to 
+        // agencies. Since they will all use "different apps". Challenging!!!
+        auth.onAuthStateChanged((user) => {
+            if (user)
+            {
+                console.log(user)
+                setUserId(user.uid)
+                const uid = user.uid;
+                console.log(userId)
+            }
+        });
+        const agencyCollection = collection(db,'agency')
+        const q = query(agencyCollection,where('agencyUsers',"array-contains",userId))
+        const agencySnap = await getDocs(q)
+        try {
+            var arr = []
+            agencySnap.forEach((doc) => {
+                arr.push({
+                    [doc.id]: doc.data(),
+                })
+            })
+            console.log(arr)
+            setAgencies(arr)
+        } catch (error) {
+            console.log(error)
+        }
+        console.log(agencies)
+        // END // NEW Work
+        // END // NEW Work
 	}
     
     const handleSearch = (e) => {
@@ -51,10 +109,6 @@ const Headbar = ({ search, setSearch, customClaims}) => {
         setSearch(e.target.value)
     }
     
-    // //
-	// Effects
-	// //
-    // Effect: get data
 	useEffect(() => {
 		getData()
 	})
@@ -64,7 +118,8 @@ const Headbar = ({ search, setSearch, customClaims}) => {
                 <div className="flex">
                 {/* TODO: - agency can swap out their logo */}
                     <div className="flex justify-center">
-                        <div className="w-10 h-10 font-extralight rounded-full tracking-widest flex justify-center text-sm items-center text-white bg-blue-500">M</div>
+                        <Image src={agencyLogo} width='100' height='100' alt='alt' />
+                        {/* <div className="w-10 h-10 font-extralight rounded-full tracking-widest flex justify-center text-sm items-center text-white bg-blue-500">M</div> */}
                     </div>
                     <div className="text-md font-semibold px-4 m-auto tracking-wide">
                         {userRole !== 'user' && `${userRole.toUpperCase()} `}
