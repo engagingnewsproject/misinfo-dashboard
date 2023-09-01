15096db18a2635c3307c8638a24a297c2d1e2a56
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-09-01 11:42:54 -0700

    working on firebase function for image uploads

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,15 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const {getStorage} = require("firebase-admin/storage");
+const mkdirp = require("mkdirp").mkdirp
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 admin.initializeApp();
-
-const path = require("path");
-
-// library for resizing images
-const sharp = require("sharp");
 // [END import]
-
+// File extension for the created JPEG files.
+const JPEG_EXTENSION = '.jpg';
 // [START generateThumbnail]
 /**
  * When an image is uploaded in the Storage bucket,
@@ -15,53 +17,42 @@ const sharp = require("sharp");
  */
 // [START generateThumbnailTrigger]
 exports.firstGenGenerateThumbnail = functions.storage.object().onFinalize(async (object) => {
-// [END generateThumbnailTrigger]
-  // [START eventAttributes]
-  const fileBucket = object.bucket; // The Storage bucket that contains the file.
-  const filePath = object.name; // File path in the bucket.
-  const contentType = object.contentType; // File content type.
-  // [END eventAttributes]
+  const filePath = object.name;
+  const baseFileName = path.basename(filePath, path.extname(filePath));
+  const fileDir = path.dirname(filePath);
+  const JPEGFilePath = path.normalize(path.format({dir: fileDir, name: baseFileName, ext: JPEG_EXTENSION}));
+  const tempLocalFile = path.join(os.tmpdir(), filePath);
+  const tempLocalDir = path.dirname(tempLocalFile);
+  const tempLocalJPEGFile = path.join(os.tmpdir(), JPEGFilePath);
 
-  // [START stopConditions]
   // Exit if this is triggered on a file that is not an image.
-  if (!contentType.startsWith('image/')) {
-    return functions.logger.log('This is not an image.');
+  if (!object.contentType.startsWith('image/')) {
+    functions.logger.log('This is not an image.');
+    return null;
   }
 
-  // Get the file name.
-  const fileName = path.basename(filePath);
-  // Exit if the image is already a thumbnail.
-  if (fileName.startsWith('thumb_')) {
-    return functions.logger.log('Already a Thumbnail.');
+  // Exit if the image is already a JPEG.
+  if (object.contentType.startsWith('image/jpeg')) {
+    functions.logger.log('Already a JPEG.');
+    return null;
   }
-  // [END stopConditions]
 
-  // [START thumbnailGeneration]
+  const bucket = admin.storage().bucket(object.bucket);
+  // Create the temp directory where the storage file will be downloaded.
+  await mkdirp(tempLocalDir);
   // Download file from bucket.
-  const bucket = admin.storage().bucket(fileBucket);
-  const metadata = {
-    contentType: contentType,
-  };
-  const downloadResponse = await bucket.file(filePath).download();
-  const imageBuffer = downloadResponse[0];
-  functions.logger.log("Image downloaded!");
-
-  // Generate a thumbnail using sharp.
-  const thumbnailBuffer = await sharp(imageBuffer).resize({
-    width: 200,
-    height: 200,
-    withoutEnlargement: true,
-  }).toBuffer();
-  functions.logger.log("Thumbnail created");
-
-  // Upload the thumbnail with a 'thumb_' prefix.
-  const thumbFileName = `thumb_${fileName}`;
-  const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
-  await bucket.file(thumbFilePath).save(thumbnailBuffer, {
-    metadata: metadata,
-  });
-  return functions.logger.log("Thumbnail uploaded!");
-  // [END thumbnailGeneration]
+  await bucket.file(filePath).download({destination: tempLocalFile});
+  functions.logger.log('The file has been downloaded to', tempLocalFile);
+  // Convert the image to JPEG using ImageMagick.
+  await spawn('convert', [tempLocalFile, tempLocalJPEGFile]);
+  functions.logger.log('JPEG image created at', tempLocalJPEGFile);
+  // Uploading the JPEG image.
+  await bucket.upload(tempLocalJPEGFile, {destination: JPEGFilePath});
+  functions.logger.log('JPEG image uploaded to Storage at', JPEGFilePath);
+  // Once the image has been converted delete the local files to free up disk space.
+  fs.unlinkSync(tempLocalJPEGFile);
+  fs.unlinkSync(tempLocalFile);
+  return null;
 });
 // [END generateThumbnail]
 
