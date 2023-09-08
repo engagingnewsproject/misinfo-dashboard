adbbaeaa2f21bcf36aa008d024c89fb7433cd924
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-09-08 16:02:20 -0700

    firstGenGenerateThumbnail image upload works with logging

--- functions/index.js
+++ functions/index.js
@@ -1,59 +1,70 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const {getStorage} = require("firebase-admin/storage");
-const mkdirp = require("mkdirp").mkdirp
-const spawn = require('child-process-promise').spawn;
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
+const mkdirp = require("mkdirp").mkdirp;
+const spawn = require("child-process-promise").spawn;
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 admin.initializeApp();
-// [END import]
-// File extension for the created JPEG files.
-const JPEG_EXTENSION = '.jpg';
-// [START generateThumbnail]
-/**
- * When an image is uploaded in the Storage bucket,
- * generate a thumbnail automatically using sharp.
- */
-// [START generateThumbnailTrigger]
-exports.firstGenGenerateThumbnail = functions.storage.object().onFinalize(async (object) => {
-  const filePath = object.name;
-  const baseFileName = path.basename(filePath, path.extname(filePath));
-  const fileDir = path.dirname(filePath);
-  const JPEGFilePath = path.normalize(path.format({dir: fileDir, name: baseFileName, ext: JPEG_EXTENSION}));
-  const tempLocalFile = path.join(os.tmpdir(), filePath);
-  const tempLocalDir = path.dirname(tempLocalFile);
-  const tempLocalJPEGFile = path.join(os.tmpdir(), JPEGFilePath);
 
-  // Exit if this is triggered on a file that is not an image.
-  if (!object.contentType.startsWith('image/')) {
-    functions.logger.log('This is not an image.');
+// File extension for the created PNG files.
+const PNG_EXTENSION = ".png";
+
+exports.firstGenGenerateThumbnail = functions.storage
+  .object()
+  .onFinalize(async (object) => {
+    const filePath = object.name;
+    const baseFileName = path.basename(filePath, path.extname(filePath));
+    const fileDir = path.dirname(filePath);
+    const PNGFilePath = path.normalize(
+      path.format({ dir: fileDir, name: baseFileName, ext: PNG_EXTENSION })
+    );
+    const tempLocalFile = path.join(os.tmpdir(),filePath);
+    functions.logger.log('1 '+tempLocalFile)
+    const tempLocalDir = path.dirname(tempLocalFile);
+    functions.logger.log('2 '+tempLocalDir)
+    const tempLocalPNGFile = path.join(os.tmpdir(), PNGFilePath);
+    functions.logger.log('3 '+tempLocalPNGFile)
+
+    if (!object.contentType.startsWith("image/")) {
+      functions.logger.log("4 This is not an image.");
+      return null;
+    }
+
+    if (object.contentType.startsWith("image/png")) {
+      functions.logger.log("5 Already a PNG.");
+      return null;
+    }
+
+    const bucket = admin.storage().bucket(object.bucket);
+
+    await mkdirp(tempLocalDir);
+
+    await bucket.file(filePath).download({ destination: tempLocalFile });
+    functions.logger.log("7 The file has been downloaded to", tempLocalFile);
+
+    // Convert the image to PNG using ImageMagick.
+    await spawn("convert", [tempLocalFile, tempLocalPNGFile]);
+    functions.logger.log("8 PNG image created at", tempLocalPNGFile);
+
+    // Uploading the PNG image.
+    await bucket.upload(tempLocalPNGFile, { destination: PNGFilePath });
+    functions.logger.log("9 PNG image uploaded to Storage at", PNGFilePath);
+    // Confirm that the temporary files are being created:
+    functions.logger.log("6 Temporary files created:", fs.existsSync(tempLocalPNGFile), fs.existsSync(tempLocalFile));
+    // Delete the originally uploaded file.
+    const originalFile = bucket.file(filePath);
+    await originalFile.delete(); // This deletes the original image file.
+
+    // confirm that the tempLocalPNGFile and tempLocalFile paths are correct and that the files exist before deletion.
+    functions.logger.log("10 Deleting files:", tempLocalPNGFile, tempLocalFile);
+
+    // Once the image has been converted, delete the local files to free up disk space.
+    fs.unlinkSync(tempLocalPNGFile);
+    fs.unlinkSync(tempLocalFile);
+
     return null;
-  }
-
-  // Exit if the image is already a JPEG.
-  if (object.contentType.startsWith('image/jpeg')) {
-    functions.logger.log('Already a JPEG.');
-    return null;
-  }
-
-  const bucket = admin.storage().bucket(object.bucket);
-  // Create the temp directory where the storage file will be downloaded.
-  await mkdirp(tempLocalDir);
-  // Download file from bucket.
-  await bucket.file(filePath).download({destination: tempLocalFile});
-  functions.logger.log('The file has been downloaded to', tempLocalFile);
-  // Convert the image to JPEG using ImageMagick.
-  await spawn('convert', [tempLocalFile, tempLocalJPEGFile]);
-  functions.logger.log('JPEG image created at', tempLocalJPEGFile);
-  // Uploading the JPEG image.
-  await bucket.upload(tempLocalJPEGFile, {destination: JPEGFilePath});
-  functions.logger.log('JPEG image uploaded to Storage at', JPEGFilePath);
-  // Once the image has been converted delete the local files to free up disk space.
-  fs.unlinkSync(tempLocalJPEGFile);
-  fs.unlinkSync(tempLocalFile);
-  return null;
-});
+  });
 // [END generateThumbnail]
 
 exports.addUserRole= functions.https.onCall((data, context)=> {
