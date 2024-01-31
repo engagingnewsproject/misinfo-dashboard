7e88515e780dc533c817285fbdf820c28ea6d385
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-01-31 15:24:17 -0800

    first fix login Form submission canceled because the form is not connected issue

--- pages/login.jsx
+++ pages/login.jsx
@@ -104,7 +104,7 @@ const Login = () => {
                 <div className="mt-5 flex-col items-center content-center">
                     <button 
                     className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mb-4 px-6 rounded focus:outline-none focus:shadow-outline"
-                    type="button" onClick={handleLogin}>
+                    type="submit" onClick={handleLogin}>
                         {loading ?        
                           <svg aria-hidden="true" className="m-auto h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">	
 									          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />	
