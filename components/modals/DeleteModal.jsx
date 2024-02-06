c59c4ef4c87a53116b1e68bd5a3dd10b2930035d
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-02-05 17:22:09 -0800

    provide users a way to delete their account

---
+++ components/modals/DeleteModal.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react"
+import { RiDeleteBin2Fill } from "react-icons/ri"
+
+const DeleteModal = ({ func, title, subtitle, CTA, closeModal }) => {
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		func()
+	}
+	return (
+		<div>
+			<div className='flex justify-center items-center z-[1900] absolute top-0 left-0 w-full h-full bg-black opacity-60'></div>
+			<div
+				className='flex justify-center items-center z-[1900] absolute top-0 left-0 w-full h-full'
+				onClick={() => closeModal(false)}>
+				<div
+					className='flex-col justify-center items-center bg-white w-80 h-auto rounded-2xl py-10 px-10'
+					onClick={(e) => {
+						e.stopPropagation()
+					}}>
+					<div className='grid justify-items-center mb-4'>
+						{CTA == "Delete" && (
+							<RiDeleteBin2Fill className='text-blue-500' size={30} />
+						)}
+						<div className='flex-col mt-3 mb-2 text-center tracking-wide'>
+							<div className='text-lg text-blue-500 font-bold my-2'>
+								{title}
+							</div>
+							<div className='text-xs font-light'>{subtitle}</div>
+						</div>
+					</div>
+					<form onSubmit={handleSubmit}>
+						<div className='mt-6 flex justify-between'>
+							<button
+								onClick={() => closeModal(false)}
+								className='bg-white hover:bg-red-500 hover:text-white text-sm text-red-500 font-bold py-1.5 px-6 rounded-md focus:outline-none focus:shadow-outline'>
+								Cancel
+							</button>
+							<button
+								className='bg-blue-500 hover:bg-white text-white text-sm hover:text-blue-500 font-bold py-1.5 px-6 rounded-md focus:outline-none focus:shadow-outline'
+								type='submit'
+								autoFocus>
+								{CTA}
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default DeleteModal
