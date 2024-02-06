9f5a8be22ed35149b937c4d4403830ae921f8059
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-02-06 12:08:16 -0800

    revert next.config

--- next.config.js
+++ next.config.js
@@ -47,14 +47,4 @@ const nextConfig = {
   }
 }
 
-// Configuration object tells the next-pwa plugin 
-const withPWA = require("next-pwa")({
-  dest: "public", // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true, // Skip waiting for service worker activation
-});
-
-// module.exports = nextConfig
-// Export the combined configuration for Next.js with PWA support
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig
