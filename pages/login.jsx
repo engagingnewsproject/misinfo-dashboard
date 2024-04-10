939823bfb4048272b957a94b955c78c5a2bbf12d
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-04-10 04:52:03 -0700

    login show hide pass

--- pages/login.jsx
+++ pages/login.jsx
@@ -7,6 +7,7 @@ import { db, auth } from '../config/firebase'
 import LanguageSwitcher from '../components/LanguageSwitcher'
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { MdOutlineRemoveRedEye } from "react-icons/md"; // <MdOutlineRemoveRedEye />
 
 const Login = () => {
   const router = useRouter()
@@ -22,6 +23,21 @@ const Login = () => {
   const [errorMessage, setErrorMessage] = useState()
   const [loading, setLoading] = useState(false)
 
+  // password show/hide
+  const [password, setPassword] = useState("")
+  const [type, setType] = useState('password')
+  const [icon, setIcon] = useState(false)
+  // handle the toggle between the hide password (eyeOff icon) and the show password (eye icon)
+  const handleTogglePass = () => {
+    if (type==='password'){
+       setIcon(true);
+       setType('text')
+    } else {
+       setIcon(false)
+       setType('password')
+    }
+ }
+ 
   //   Get user custom token
   useEffect(() => {
     // Prefetch the dashboard page
@@ -29,46 +45,47 @@ const Login = () => {
   }, [router])
 
   const handleLogin = async (e) => {
-		e.preventDefault()
-		setLoading(true) // Set loading state to true during login process
-		try {
-			await login(data.email, data.password)
-			// Login successful, check if email is verified
-			if (auth.currentUser?.emailVerified) {
-				const idTokenResult = await auth.currentUser.getIdTokenResult()
-				console.log("Claims:", idTokenResult.claims)
+    e.preventDefault()
+    setLoading(true) // Set loading state to true during login process
+    try {
+      await login(data.email, data.password)
+      // Login successful, check if email is verified
+      if (auth.currentUser?.emailVerified) {
+        const idTokenResult = await auth.currentUser.getIdTokenResult()
+        console.log("Claims:", idTokenResult.claims)
 
-				// Redirect user based on their role
-				if (idTokenResult.claims.admin || idTokenResult.claims.agency) {
-					console.log("Redirecting to dashboard...")
-					await router.push("/dashboard")
-				} else {
-					console.log("Redirecting to report page...")
-					await router.push("/report")
-				}
-			} else {
-				// Email not verified, send verification email and redirect
-				console.log("Email not verified. Sending verification email...")
-				await verifyEmail(auth.currentUser)
-				await router.push("/verifyEmail")
-			}
+        // Redirect user based on their role
+        if (idTokenResult.claims.admin || idTokenResult.claims.agency) {
+          console.log("Redirecting to dashboard...")
+          await router.push("/dashboard")
+        } else {
+          console.log("Redirecting to report page...")
+          await router.push("/report")
+        }
+      } else {
+        // Email not verified, send verification email and redirect
+        console.log("Email not verified. Sending verification email...")
+        await verifyEmail(auth.currentUser)
+        await router.push("/verifyEmail")
+      }
       analytics.logEvent('login', { method: 'email' }); // Log 'login' event
-		} catch (error) {
-			// Login error occurred, handle and display it
-			console.error("Login error:", error)
-			if (error.code === "auth/user-not-found") {
-				setError(t("not_found"))
-			} else if (error.code === "auth/wrong-password") {
-				setError(t("incorrect"))
-			} else {
-				setError(t("error"))
-			}
+    } catch (error) {
+      // Login error occurred, handle and display it
+      console.error("Login error:", error)
+      if (error.code === "auth/user-not-found") {
+        setError(t("not_found"))
+      } else if (error.code === "auth/wrong-password") {
+        setError(t("incorrect"))
+      } else {
+        setError(t("error"))
+      }
     }
     setLoading(false)
-	}
+  }
 
   const handleChange = (e) => {
-      setData({ ...data, [e.target.id]: e.target.value})
+    setPassword(e.target.value)
+    setData({ ...data, [e.target.id]: e.target.value})
   }
 
 
@@ -91,17 +108,21 @@ const Login = () => {
                         autoComplete='username'
                         />
                 </div>
-                <div className="mb-1">
+                <div className="mb-1 flex">
                     <input
                         className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline"
                         id="password"
-                        type="password"
+                        type={type}
+                        name='password'
                         placeholder={t("password")}
                         required 
                         value={data.password}
                         onChange={handleChange}
                         autoComplete='current-password'
                         />
+                        <span class="flex justify-around items-center" onClick={handleTogglePass}>
+                          <MdOutlineRemoveRedEye className='absolute mr-10' />
+                        </span>
                 </div>
                 {error && <span className="text-red-500 text-sm font-light">{error}</span>}
                 <div className="mt-5 flex-col items-center content-center">
@@ -110,8 +131,8 @@ const Login = () => {
                     type="submit">
                         {loading ?        
                           <svg aria-hidden="true" className="m-auto h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">	
-									          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />	
-									          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />	
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />	
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />	
                           </svg>
                               
                           : t("login") }
