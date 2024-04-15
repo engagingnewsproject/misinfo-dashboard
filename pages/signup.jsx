e38c146615b85aa82c49312ae00288d614389e2f
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-04-15 04:51:46 -0700

    signup.jsx remove isAgency TESTING

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -39,7 +39,7 @@ const SignUp = () => {
     const addMobileUser = (privilege) => {
         // Get user object
         const user = auth.currentUser;
-        
+        console.log(user)
         if (user) {
             // Set user uid
             // console.log("adding mobile user")
@@ -73,7 +73,7 @@ const SignUp = () => {
         // console.log("should be given agency privilege " + isAgency)
 
           try {
-              if (isAgency) {
+              // if (isAgency) {
 
                 // Sees if agency already exists -if it does, adds user to the agency's user list
                   signInWithEmailLink(auth, data.email, window.location.href).then((result) =>{
@@ -93,7 +93,7 @@ const SignUp = () => {
                       if (verifyEmail(auth.currentUser)) {
                         setSignUpError("")
                         // console.log("in try")
-                        window.location.replace('/dashboard')
+                        window.location.replace('/verifyEmail')
                       } else {
                         // console.log("here = for agency")
                         addMobileUser("Agency")
@@ -112,32 +112,32 @@ const SignUp = () => {
                     }
                   })
 
-                  const userCredential = await auth.currentUser.linkWithCredential(result.credential);
+                  // const userCredential = await auth.currentUser.linkWithCredential(result.credential);
 
                   verifyEmail(auth.currentUser).then((verified) => {
                     // Handle email verification logic
                     // ...
                   });
                 
-              } else {
-                signup(data.name, data.email, data.password)
-									.then((userCredential) => {
-										setSignUpError("")
-                    addMobileUser("User")
-										router.push('/verifyEmail');
-									})
-									.catch((error) => {
-										if (error.code === "auth/email-already-in-use") {
-											setSignUpError("The entered email is already in use.")
-										} else {
-											setSignUpError(error.message)
-										}
-										console.error(error)
-									})
+              // } else {
+              //   signup(data.name, data.email, data.password)
+							// 		.then(() => {
+							// 			setSignUpError("")
+              //       addMobileUser("User")
+							// 			router.push('/verifyEmail');
+							// 		})
+							// 		.catch((error) => {
+							// 			if (error.code === "auth/email-already-in-use") {
+							// 				setSignUpError("The entered email is already in use.")
+							// 			} else {
+							// 				setSignUpError(error.message)
+							// 			}
+							// 			console.error(error)
+							// 		})
 
 
-              }
-              analytics.logEvent('sign_up', { method: 'email' }); // Log 'login' event
+              // }
+              // analytics.logEvent('sign_up', { method: 'email' }); // Log 'login' event
           } catch (err) {
               
               if (err.message == "Firebase: Error (auth/email-already-in-use).") {
@@ -172,28 +172,25 @@ const SignUp = () => {
                 </div>
                 <form className="px-8 pt-6 pb-4 mb-4"  onSubmit={handleSignUp}>
                     <div className="mb-4">
-                        {/* Only allows user to select team name. Agencies had already had their name selected. */}  
-                        {!isAgency &&       
-                          <input
-                            className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="name"
-                            type="text"
-                            placeholder={t("name")}
-                            required
-                            value={data.name}
-                            onChange={handleChange}
-                            autoComplete=''
-                            />
-                        }
+                        <input
+                          className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          id="name"
+                          type="text"
+                          placeholder={t("name")}
+                          required
+                          value={data.name}
+                          onChange={handleChange}
+                          autoComplete=''
+                          />
                     </div>
                     <div className="mb-4">
               
                           {/* TODO: {agency && Instructions for an agency to sign up */}
-                    {isAgency && 
+                    {/* {isAgency && 
                     <div>
                       <p className="text-lg font-bold text-blue-600 tracking-wider pt-2">Account Creation</p>
                       <div className="mb-1">Enter your email address.</div>
-                    </div>}
+                    </div>} */}
                     <PhoneInput
                         placeholder={t("phone")}
                         value={data.phone}
@@ -215,8 +212,8 @@ const SignUp = () => {
                         />
                     </div>
                     <div className="mb-1">
-                      {isAgency && 
-                        <div className="mb-1">Create a secure password for your account.</div>}
+                      {/* {isAgency && 
+                        <div className="mb-1">Create a secure password for your account.</div>} */}
                         <input
                             className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             id="password"
