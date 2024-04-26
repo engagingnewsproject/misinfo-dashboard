0dbfa0ba7fbc624d92a21dc14e58ea8120c95919
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-04-26 04:19:57 -0700

    button style: hover

--- components/ReportLanding.jsx
+++ components/ReportLanding.jsx
@@ -40,7 +40,7 @@ const ReportLanding = ({
 		container: "z-0 flex-col lg:max-w-4xl",
 		headerWrap: 'flex pb-4 justify-between',
 		header: "text-center md:text-left text-xl font-bold text-blue-600 tracking-wider mt-2",
-		buttonLg: 'flex items-center justify-center gap-5 bg-blue-600 w-full hover:bg-blue-200 text-white font-normal py-2 px-6 border border-blue-600 rounded-xl',
+		buttonLg: 'flex items-center justify-center gap-5 bg-blue-600 w-full hover:bg-blue-500 text-white font-normal py-2 px-6 border border-blue-600 rounded-xl',
 		button: 'bg-sky-100 hover:bg-blue-200 text-blue-600 font-normal py-2 px-6 mt-4 border border-blue-600 rounded-xl',
 		systemWrap: 'text-xl font-extrabold text-blue-600 tracking-wider mt-5'
 	}
