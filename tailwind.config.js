091d754ccb87507327178bb28bf8feeec9d3459b
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-05-10 15:59:35 -0700

    material tailwind install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          '600': '#2563eb', // Override the default blue-600 color
+          // Optionally, you can define other shades of blue as well
+          '700': '#173ead',
+          // Add more shades as needed
+        },
+        sky: {
+          '100': '#e0f2fe'
+        }
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
   ],
-}
+});
