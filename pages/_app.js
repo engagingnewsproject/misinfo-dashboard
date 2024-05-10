7fcab09e1a1d9aa8af8424af2f109f45308edff8
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-05-10 16:00:18 -0700

    ThemeProvider - Material Tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import Head from 'next/head'
 import '../styles/globals.css'
 import 'react-tooltip/dist/react-tooltip.css'
 import { appWithTranslation } from 'next-i18next';
+import { ThemeProvider } from '@material-tailwind/react';
 
 // Define PWA metadata
 // https://medium.com/readytowork-org/implementing-pwa-in-the-next-13-application-3e46f6b3f6d8
@@ -51,67 +52,68 @@ function MyApp({ Component, pageProps }) {
   }, []);
   
   return (
-    <AuthContextProvider>
-      <Head>
-        <meta name="application-name" content="PWA App" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PWA App" />
-        <meta name="description" content="Best PWA App in the world" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#2B5797" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
-        
-        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#2066d3" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
+    <ThemeProvider>
+      <AuthContextProvider>
+        <Head>
+          <meta name="application-name" content="PWA App" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="PWA App" />
+          <meta name="description" content="Best PWA App in the world" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+          
+          <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#2066d3" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
 
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="shortcut icon" href="/icons/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/icons/favicon.ico" />
 
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <link href="/icons/apple_splash_2048.png" sizes="2048x2732" rel="apple-touch-startup-image" />
-        <link href="/icons/apple_splash_1668.png" sizes="1668x2224" rel="apple-touch-startup-image" />
-        <link href="/icons/apple_splash_1536.png" sizes="1536x2048" rel="apple-touch-startup-image" />
-        <link href="/icons/apple_splash_1125.png" sizes="1125x2436" rel="apple-touch-startup-image" />
-        <link href="/icons/apple_splash_1242.png" sizes="1242x2208" rel="apple-touch-startup-image" />
-        <link href="/icons/apple_splash_750.png" sizes="750x1334" rel="apple-touch-startup-image" />
-        <link href="/icons/apple_splash_640.png" sizes="640x1136" rel="apple-touch-startup-image" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://misinfo-dashboard.netlify.app" />
-        <meta name="twitter:title" content="Misinformation App" />
-        <meta name="twitter:description" content="Report misinformation in your location for submission to news agencies near that location." />
-        <meta name="twitter:image" content="/icons/android-chrome-192x192.png" />
-        <meta name="twitter:creator" content="@CME" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Misinformation App" />
-        <meta property="og:description" content="Report misinformation in your location for submission to news agencies near that location" />
-        <meta property="og:site_name" content="Misinformation App" />
-        <meta property="og:url" content="https://misinfo-dashboard.netlify.app" />
-        <meta property="og:image" content="https://misinfo-dashboard.netlify.app/icons/apple-touch-icon.png" />
-      </Head>
-      <div className="bg-sky-100 w-full h-full overflow-y-auto">
-        <div className='w-screen h-screen content-center'>
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link href="/icons/apple_splash_2048.png" sizes="2048x2732" rel="apple-touch-startup-image" />
+          <link href="/icons/apple_splash_1668.png" sizes="1668x2224" rel="apple-touch-startup-image" />
+          <link href="/icons/apple_splash_1536.png" sizes="1536x2048" rel="apple-touch-startup-image" />
+          <link href="/icons/apple_splash_1125.png" sizes="1125x2436" rel="apple-touch-startup-image" />
+          <link href="/icons/apple_splash_1242.png" sizes="1242x2208" rel="apple-touch-startup-image" />
+          <link href="/icons/apple_splash_750.png" sizes="750x1334" rel="apple-touch-startup-image" />
+          <link href="/icons/apple_splash_640.png" sizes="640x1136" rel="apple-touch-startup-image" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://misinfo-dashboard.netlify.app" />
+          <meta name="twitter:title" content="Misinformation App" />
+          <meta name="twitter:description" content="Report misinformation in your location for submission to news agencies near that location." />
+          <meta name="twitter:image" content="/icons/android-chrome-192x192.png" />
+          <meta name="twitter:creator" content="@CME" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Misinformation App" />
+          <meta property="og:description" content="Report misinformation in your location for submission to news agencies near that location" />
+          <meta property="og:site_name" content="Misinformation App" />
+          <meta property="og:url" content="https://misinfo-dashboard.netlify.app" />
+          <meta property="og:image" content="https://misinfo-dashboard.netlify.app/icons/apple-touch-icon.png" />
+        </Head>
+        <div className="bg-sky-100 w-full h-full overflow-y-auto">
+          <div className='w-screen h-screen content-center'>
 
-          { noAuthRequired.includes(router.pathname) ? (
-            <Component {...pageProps} />
-          ) : (
-            <ProtectedRoute>
+            { noAuthRequired.includes(router.pathname) ? (
               <Component {...pageProps} />
+            ) : (
+              <ProtectedRoute>
+                <Component {...pageProps} />
 
-            </ProtectedRoute>
-          ) }
+              </ProtectedRoute>
+            ) }
 
+          </div>
         </div>
-      </div>
-    </AuthContextProvider>
-    
+      </AuthContextProvider>
+    </ThemeProvider>
   )
 }
 
