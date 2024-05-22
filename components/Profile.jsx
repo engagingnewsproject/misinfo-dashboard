aef817241e9191b58a85f305f95816f6a93414d2
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-05-22 09:58:09 -0700

    profile location update mv own file

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -28,6 +28,7 @@ import Select from 'react-select';
 import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
 import globalStyles from '../styles/globalStyles';
+import LocationUpdate from './partials/forms/LocationUpdate';
 // Profile page that allows user to edit password or logout of their account
 const Profile = ({ customClaims }) => {
   const {
@@ -149,81 +150,6 @@ const Profile = ({ customClaims }) => {
       }
     });
   }, []); // this useEffect call will run on page load, since it has no arguments in the []. If arguments are present in the [], effect will only activate if the values in the list change.
-
-  // LOCATION CHANGE FOR USERS
-  // handle when a user clicks the "Change Location" button
-  const handleChangeLocation = () => {
-    setUserLocationChange(true);
-  };
-
-  const handleUserStateChange = (e) => {
-    setShowUserMessage(false);
-    setUserLocation((data) => ({ ...data, state: e, city: null }));
-  };
-  const handleUserCityChange = (e) => {
-    setShowUserMessage(false);
-    setUserLocation((data) => ({ ...data, city: e !== null ? e : null }));
-  };
-
-  // handle location reset, delete changes for general users
-  const handleUserLocationReset = () => {
-    setShowUserMessage(false);
-    getData() // fetch data again
-    // reset to default firestore values
-    setUserLocation({ state: userData?.state, city: userData?.city });
-  };
-  // handle location change for users
-  const handleUserLocationChange = (e) => {
-    e.preventDefault(); // prevents the page from refreshing
-    const allErrors = {};
-    // error logging
-    if (userLocation === null) {
-      allErrors.userState = 'Please enter a state.';
-    } else if (!userLocation.city) {
-      allErrors.userCity = 'Please enter a city.';
-    } else {
-      try {
-        // set the userData state
-        setUserData({
-          state: userLocation?.state,
-          city: userLocation?.city
-        })
-        // where we update the firestore data
-        const userDoc = doc(db,'mobileUsers',user.accountId);
-        // update existing fields without overwriting everything
-        updateDoc(userDoc, {
-          state: userLocation.state,
-          city: userLocation.city,
-        }).then(() => {
-          // update state variables
-          getData();
-        });
-        // set userUpdate state (see useEffect)
-        setUserUpdate(true);
-        setShowUserMessage(!showUserMessage);
-        // Set state or do any necessary actions after successfully updating Firestore
-      } catch (error) {
-        console.error('Error updating user location:', error);
-      }
-    }
-    // set all errors
-    setErrors(allErrors);
-  };
-
-  // When user location updated we can set the user location change to false and it will close the form block
-  useEffect(() => {
-    setUserLocationChange(false); // hides the userLocation form
-    setTimeout(() => {
-      setShowUserMessage(false); // delay the success message
-    }, 3000);
-  }, [userUpdate]); // when userUpdate state changes
-
-  
-  useEffect(() => {
-    // if user adds a city this will hide the error message
-    userLocation?.city &&
-      setErrors([])
-  }, [userLocation])
   
   // SAVE AGENCY
   const saveAgency = (imageURLs) => {
@@ -530,112 +456,8 @@ const Profile = ({ customClaims }) => {
         </div>
 
         {/* User Location Edit*/}
-        <div>
-          <div className="text-xl font-extrabold text-blue-600">
-            {t('editLocation')}
-          </div>
-          {/* List the user's location information */}
-          <div className="flex justify-between mx-0 md:mx-6 my-6 tracking-normal items-center">
-            <div className="font-light">City</div>
-            {/* check if user data has a value */}
-            {userData && userData.city && (
-              <div className="font-light">{userData.city.name}</div>
-            )}
-          </div>
-          <div className="flex justify-between mx-0 md:mx-6 my-6 tracking-normal items-center">
-            <div className="font-light">State</div>
-            {userData && userData.state && (
-              <div className="font-light">{userData.state.name}</div>
-            )}
-          </div>
-          {/* show/hide change location fields */}
-          <>
-            {userLocationChange && ( // show form!
-              <form onSubmit={handleUserLocationChange} ref={formRef}>
-                {/* Need to wrap any form elements in a form tag */}
-                <div className="flex justify-between mx-0 md:mx-6 my-6 tracking-normal items-center">
-                  <div className="flex flex-auto justify-between">
-                    {/* These could be changed to the tailwindcss material design select elements */}
-                    <Select
-                      className="border-white rounded-md w-full text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline px-2"
-                      id="state"
-                      type="text"
-                      required
-                      placeholder={t('NewReport:state_text')}
-                      value={userLocation?.state}
-                      options={State.getStatesOfCountry('US')}
-                      getOptionLabel={(options) => {
-                        return options['name'];
-                      }}
-                      getOptionValue={(options) => {
-                        return options['name'];
-                      }}
-                      label="state"
-                      onChange={handleUserStateChange}
-                    />
-                    <Select
-                      className="shadow border-white rounded-md w-full text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline px-2"
-                      id="city"
-                      type="text"
-                      placeholder={t('NewReport:city_text')}
-                      value={userLocation?.city}
-                      options={City.getCitiesOfState(
-                        userLocation?.state?.countryCode,
-                        userLocation?.state?.isoCode
-                      )}
-                      getOptionLabel={(options) => {
-                        return options['name'];
-                      }}
-                      getOptionValue={(options) => {
-                        return options['name'];
-                      }}
-                      onChange={handleUserCityChange}
-                    />
-                  </div>
-                  <div>
-                    <button
-                      onClick={handleUserLocationReset}
-                      className={`${style.button}`}>
-                      {t('cancelChanges')}
-                    </button>
-                    <button
-                      className={`${style.button}`}
-                      type="submit">
-                      {t('updateLocation')}
-                    </button>
-                  </div>
-                </div>
-              </form>
-            )}
-            {/* Change location button */}
-            <div className="flex justify-end mx-0 md:mx-6 my-6 tracking-normal items-center">
-              {showUserMessage && (
-                <p className="text-green-800 transition-opacity opacity-100 mr-4">
-                  {t('location')}
-                </p>
-              )}
-              {/* Error output */}
-              {errors.userState && data.state === null && (
-                <div className="flex justify-center mr-4">
-                  <span className="text-red-500">{errors.userState}</span>
-                </div>
-              )}
-              {errors.userCity && data.city === null && (
-                <div className="flex justify-center mr-4">
-                  <span className="text-red-500">{errors.userCity}</span>
-                </div>
-              )}
-              {/* if form is not visible show this button */}
-              {!userLocationChange && (
-                <button
-                  className={`${style.buttonHollow} justify-end`}
-                  onClick={() => handleChangeLocation()}>
-                  Change Location
-                </button>
-              )}
-            </div>
-          </>
-        </div>
+
+        <LocationUpdate user={user} userData={userData} setUserData={setUserData} />
 
         {/* Hides the language toggle for admin and agencies*/}
         {!isAgency && !isAdmin && (
