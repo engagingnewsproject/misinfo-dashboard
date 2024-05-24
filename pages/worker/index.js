a1558f94cdf232727598775773d1e65a7069f179
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-05-23 17:27:01 -0700

    netlify fix: self is not defined

--- pages/worker/index.js
+++ pages/worker/index.js
@@ -7,13 +7,14 @@
 const util = require('./util')
 
 util()
-
-// listen to message event from window
-self.addEventListener('message', event => {
-  // HOW TO TEST THIS?
-  // Run this in your browser console: 
-  //     window.navigator.serviceWorker.controller.postMessage({command: 'log', message: 'hello world'})
-  // OR use next-pwa injected workbox object
-  //     window.workbox.messageSW({command: 'log', message: 'hello world'})
-  console.log(event.data)
-})
+if (typeof window !== 'undefined') {
+  // listen to message event from window
+  self.addEventListener('message',event => {
+    // HOW TO TEST THIS?
+    // Run this in your browser console: 
+    //     window.navigator.serviceWorker.controller.postMessage({command: 'log', message: 'hello world'})
+    // OR use next-pwa injected workbox object
+    //     window.workbox.messageSW({command: 'log', message: 'hello world'})
+    console.log(event.data)
+  })
+}
