33b3d32124be9f89e1f7b79bcf582c180dedf045
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-05-23 17:07:12 -0700

    pwa fix: https://github.com/shadowwalker/next-pwa/blob/master/examples/offline-fallback-v2/README.md

--- pages/worker/index.js
+++ pages/worker/index.js
@@ -3,7 +3,6 @@
 // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
 //
-self.__WB_DISABLE_DEV_LOGS = true
 
 const util = require('./util')
 
