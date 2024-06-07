aeab871bee0435e6f6efec7d53ec31c3e547d54f
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-06-07 12:28:21 -0700

    how to use useNetworkStatus hook

--- hooks/useNetworkStatus.js
+++ hooks/useNetworkStatus.js
@@ -1,4 +1,25 @@
-import { useEffect, useState } from 'react';
+/*
+Use `useNetworkStatus` on component to monitor network status...
+1) Import: 
+  import useNetworkStatus from '../hooks/useNetworkStatus';
+  
+2) Set Const: 
+  const isOnline = useNetworkStatus();
+  
+3) UseEffect:
+	useEffect(() => {
+		if (!isOnline) {
+			console.log('The browser is offline.')
+		}
+	}, [isOnline])
+
+4) Add Markup:
+  {process.env.NODE_ENV === 'development' && (
+    <div className={`text-center font-extrabold text-lg ${isOnline ? 'text-green-600' : 'text-red-800'}`}>{isOnline ? "Online" : "Offline"}</div>
+  )}
+*/
+
+import { useEffect,useState } from 'react';
 
 const useNetworkStatus = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
