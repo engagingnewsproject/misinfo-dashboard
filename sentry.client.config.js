bccf503f3607b3b28953c7c7ae1ac1af9cf5e4cc
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-08-08 13:01:53 -0700

    disable sentry on local dev

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -4,29 +4,31 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://845c7860c580e1b6c2fa9ce325d1257c@o204741.ingest.us.sentry.io/4507670446931968",
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn: "https://845c7860c580e1b6c2fa9ce325d1257c@o204741.ingest.us.sentry.io/4507670446931968",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-   replayIntegration: function() {
-     // Your implementation here
-   },
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  // integrations: [
-  //   Sentry.replayIntegration({
-  //     // Additional SDK configuration goes in here, for example:
-  //     maskAllText: true,
-  //     blockAllMedia: true,
-  //   }),
-  // ],
-});
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
+    replayIntegration: function () {
+      // Your implementation here
+    },
+    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+    // integrations: [
+    //   Sentry.replayIntegration({
+    //     // Additional SDK configuration goes in here, for example:
+    //     maskAllText: true,
+    //     blockAllMedia: true,
+    //   }),
+    // ],
+  })
+}
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -3,17 +3,18 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn: "https://845c7860c580e1b6c2fa9ce325d1257c@o204741.ingest.us.sentry.io/4507670446931968",
 
-Sentry.init({
-  dsn: "https://845c7860c580e1b6c2fa9ce325d1257c@o204741.ingest.us.sentry.io/4507670446931968",
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
+    // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: process.env.NODE_ENV === 'development',
   
-});
+  })
+}
