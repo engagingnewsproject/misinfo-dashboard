88b93657bc7868f2739aa8b5306a087e7a92437e
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-08-08 13:51:46 -0700

    fix agency modal send new user an email issue

--- components/modals/AgencyModal.jsx
+++ components/modals/AgencyModal.jsx
@@ -89,10 +89,10 @@ const handleImageChange = (e) => {
 				setSendEmail(`${userEmail} is already an admin for this agency.`)
 				break
 			}
-      // await sendSignIn(userEmail)
+      await sendSignIn(userEmail)
       // setSendEmail("Sign-in link was sent.")
 
-    }
+		}
     const agencyRef = doc(db, "agency", agencyId);
 		updateDoc(agencyRef, {
       agencyUsers: arrayUnion(...tempUsersArr),
@@ -183,7 +183,6 @@ const handleImageChange = (e) => {
       ...agencyUsersArr.slice(0, agencyUsersArr.indexOf(adminDelete)),
       ...agencyUsersArr.slice(agencyUsersArr.indexOf(adminDelete) + 1)
     ]
-    console.log(tempUsersArr)
     const agencyRef = doc(db, "agency", agencyId);
 		updateDoc(agencyRef, {
       agencyUsers: tempUsersArr,
