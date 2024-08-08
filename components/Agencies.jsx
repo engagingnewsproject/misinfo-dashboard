5315012f13bd7695e4bfc2eda99fafb0fa4f0f1d
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-08-07 19:29:55 -0700

    fix agencies missing send sign in function

--- components/Agencies.jsx
+++ components/Agencies.jsx
@@ -39,7 +39,7 @@ const Agencies = ({handleAgencyUpdateSubmit}) => {
 	const [endIndex, setEndIndex] = useState(10)
 	const [deleteModal, setDeleteModal] = useState(false)
 	// NEW Agency Modal
-  const { user } = useAuth()
+  const { user, sendSignIn } = useAuth()
 	const [newAgencyModal, setNewAgencyModal] = useState(false)
 	const [newAgencySubmitted, setNewAgencySubmitted] = useState(0);
 	const [newAgencyName, setNewAgencyName] = useState("")
