6d54af58c79a131db063be29e37862157ea79111
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-09-11 12:13:38 -0700

    login page title fix

--- pages/login.jsx
+++ pages/login.jsx
@@ -18,6 +18,7 @@ import {
 	doc,
 } from "firebase/firestore"
 import { GiMagnifyingGlass } from "react-icons/gi";
+import Head from 'next/head';
 
 const Login = () => {
   const router = useRouter()
@@ -122,84 +123,112 @@ const Login = () => {
 
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-full max-w-sm font-light">
-        <div className="flex justify-center h-auto mb-4">
-            <div className='bg-blue-600 p-7 rounded-full'>
-              <GiMagnifyingGlass size={ 30 } className='fill-white' />
-            </div>
-        </div>
-        <form className="px-8 pt-6 pb-4 mb-4" onSubmit={handleLogin}>
-            <div className="mb-4">
-                <input
-                    className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email"
-                    type="text"
-                    placeholder={t("email")}
-                    required
-                    value={data.email}
-                    onChange={handleChange}
-                    autoComplete='username'
-                    />
-            </div>
-            <div className="mb-1 flex">
-                <input
-                    className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline"
-                    id="password"
-                    type={type}
-                    name='password'
-                    placeholder={t("password")}
-                    required 
-                    value={data.password}
-                    onChange={handleChange}
-                    autoComplete='current-password'
-                    />
-                    <span className="flex justify-around items-center" onClick={handleTogglePass}>
-                      <MdOutlineRemoveRedEye className='absolute mr-10' />
-                    </span>
-            </div>
-            {error && <span className="text-red-500 text-sm font-light">{error}</span>}
-            <div className="mt-5 flex-col items-center content-center">
-                <button 
-                className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mb-4 px-6 rounded focus:outline-none focus:shadow-outline"
-                type="submit">
-                    {loading ?        
-                      <svg aria-hidden="true" className="m-auto h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">	
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />	
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />	
-                      </svg>
-                          
-                      : t("login") }
-                </button>
-                <div className="flex items-center justify-between">
-                    <div className="content-center">
-                        <input type="checkbox" name='remember-me' className="form-checkbox rounded-sm border-transparent focus:border-transparent focus:ring-0" onChange={handleChange} />
-                        <span className="text-sm p-2">{t("remember")}</span>
-                    </div>
-                    <Link href="/resetPassword" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
-                      {t('forgot')}
-
-                    </Link>
-                </div>
-      
-            </div>
-        </form>
-        <p className="text-center text-gray-500 text-sm">
-            {t("noAccount")}
-            <Link href="/signup" className="inline-block px-2 align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
-                {t('signup')}
-            </Link>
-        </p>
-        {/* <View> */}
-        <div className="flex justify-between items-center p-6 gap-1">
-          <span className="text-blue-500 text-md uppercase font-bold py-2 px-2">{t("select")}</span>
-          <LanguageSwitcher/>
-        </div>
-
-      </div>
-
-    </div>
-  )
+		<>
+			<Head>
+				<title>Login | Truth Sleuth Local</title>
+			</Head>
+			<div className="w-screen h-screen flex justify-center items-center">
+				<div className="w-full max-w-sm font-light">
+					<div className="flex justify-center h-auto mb-4">
+						<div className="bg-blue-600 p-7 rounded-full">
+							<GiMagnifyingGlass size={30} className="fill-white" />
+						</div>
+					</div>
+					<form className="px-8 pt-6 pb-4 mb-4" onSubmit={handleLogin}>
+						<div className="mb-4">
+							<input
+								className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="email"
+								type="text"
+								placeholder={t('email')}
+								required
+								value={data.email}
+								onChange={handleChange}
+								autoComplete="username"
+							/>
+						</div>
+						<div className="mb-1 flex">
+							<input
+								className="shadow border-white rounded-md w-full py-3 px-3 text-sm text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline"
+								id="password"
+								type={type}
+								name="password"
+								placeholder={t('password')}
+								required
+								value={data.password}
+								onChange={handleChange}
+								autoComplete="current-password"
+							/>
+							<span
+								className="flex justify-around items-center"
+								onClick={handleTogglePass}>
+								<MdOutlineRemoveRedEye className="absolute mr-10" />
+							</span>
+						</div>
+						{error && (
+							<span className="text-red-500 text-sm font-light">{error}</span>
+						)}
+						<div className="mt-5 flex-col items-center content-center">
+							<button
+								className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mb-4 px-6 rounded focus:outline-none focus:shadow-outline"
+								type="submit">
+								{loading ? (
+									<svg
+										aria-hidden="true"
+										className="m-auto h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+										viewBox="0 0 100 101"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg">
+										<path
+											d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+											fill="currentColor"
+										/>
+										<path
+											d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+											fill="currentFill"
+										/>
+									</svg>
+								) : (
+									t('login')
+								)}
+							</button>
+							<div className="flex items-center justify-between">
+								<div className="content-center">
+									<input
+										type="checkbox"
+										name="remember-me"
+										className="form-checkbox rounded-sm border-transparent focus:border-transparent focus:ring-0"
+										onChange={handleChange}
+									/>
+									<span className="text-sm p-2">{t('remember')}</span>
+								</div>
+								<Link
+									href="/resetPassword"
+									className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+									{t('forgot')}
+								</Link>
+							</div>
+						</div>
+					</form>
+					<p className="text-center text-gray-500 text-sm">
+						{t('noAccount')}
+						<Link
+							href="/signup"
+							className="inline-block px-2 align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+							{t('signup')}
+						</Link>
+					</p>
+					{/* <View> */}
+					<div className="flex justify-between items-center p-6 gap-1">
+						<span className="text-blue-500 text-md uppercase font-bold py-2 px-2">
+							{t('select')}
+						</span>
+						<LanguageSwitcher />
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Login;
