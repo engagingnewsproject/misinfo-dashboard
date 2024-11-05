a03e93ae666da931769c69cc8c788a61ca501f13
Luke C Hartman <luke@lukecarlhartman.com>
2024-11-04 21:41:23 -0800

    tooltip memoized

---
+++ components/partials/table/MemoizedTooltipContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Tooltip, Typography } from '@material-tailwind/react'
+
+// Memoized Tooltip Component
+const MemoizedTooltipContent = React.memo(({ details }) => (
+  <div className="w-80">
+    <Typography color="white" className="font-normal opacity-80">
+      Description: {details}
+    </Typography>
+  </div>
+))
+
+export default MemoizedTooltipContent
