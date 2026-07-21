f0d8bd0d3b6ed2d713bdf9fa6cc771878e15246b
Luke C Hartman <luke@lukecarlhartman.com>
2026-07-21 15:26:56 -0700

    style(agency-modal): update button size for improved UI consistency

--- components/modals/admin/AgencyModal.jsx
+++ components/modals/admin/AgencyModal.jsx
@@ -183,7 +183,9 @@ const AgencyModal = ({
 									actionText="Choose image"
 								/>
 							</div>
-							<Button type="submit">Update Agency</Button>
+							<Button type="submit" size="sm">
+								Update Agency
+							</Button>
 						</div>
 					</form>
 				</DialogBody>
